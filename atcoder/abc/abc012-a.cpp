db05318f693de06758e2f456bf88c109877bc1d7
nao109 <haruki.2004.109@gmail.com>
2022-02-05 17:04:17 +0900

    solve abc011-020-a

---
+++ atcoder/abc/abc012-a.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+int main(){
+  cout << fixed << setprecision(7);
+  int a,b; cin >> a >> b;
+  cout << b << " " << a << endl;
+  return 0;
+}

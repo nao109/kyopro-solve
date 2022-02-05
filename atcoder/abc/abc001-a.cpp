e2105935e746c4d205cf4670fe56daa257b19e4b
nao109 <haruki.2004.109@gmail.com>
2022-02-05 16:02:59 +0900

    solve abc001-010-a

---
+++ atcoder/abc/abc001-a.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+int main(){
+  int h1,h2; cin >> h1 >> h2;
+  cout << h1-h2 << endl;
+  return 0;
+}

33ed8c1f4d91eb3ffea768be2507906fd011240d
nao109 <80878102+nao109@users.noreply.github.com>
2022-03-03 17:54:40 +0900

    solve abc231-238-a

---
+++ atcoder/abc/abc237-a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+int main(){
+  cout << fixed << setprecision(7);
+  ll n; cin >> n;
+  ll ch=pow(2,31);
+  if(-ch<=n&&n<ch) cout << "Yes\n";
+  else cout << "No\n";
+  return 0;
+}

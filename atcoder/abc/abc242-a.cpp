0721cf1f17c38ab2fb75b54f49782864404f976a
nao109 <haruki.2004.109@gmail.com>
2022-03-06 17:57:41 +0900

    solve abc242-a,b

---
+++ atcoder/abc/abc242-a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+int main(){
+  cout << fixed << setprecision(7);
+  double a,b,c,x; cin >> a >> b >> c >> x;
+  if(x<=a) cout << 1 << endl;
+  else if(x<=b) cout << c/(b-a) << endl;
+  else cout << 0 << endl;
+  return 0;
+}
---
+++ atcoder/abc/abc242-b.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+int main(){
+  cout << fixed << setprecision(7);
+  string s; cin >> s;
+  sort(s.begin(),s.end());
+  cout << s << endl;
+  return 0;
+}

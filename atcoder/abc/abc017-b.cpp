3d80dbc9c4a3cc88f33389e88b43714d19084516
nao109 <80878102+nao109@users.noreply.github.com>
2022-03-09 13:44:41 +0900

    solve abc017-b

--- atcoder/abc/abc017-b.cpp
+++ atcoder/abc/abc017-b.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep(i,n) for(int i=0; i<(n); i++)
 int main(){
   cout << fixed << setprecision(7);
   string s[4]={"ch","o","k","u"};
@@ -12,12 +12,25 @@ int main(){
     rep(i,4){
       if(t.find(s[i])!=string::npos){
         ch=1;
-        if(i==0) t.erase(t.find(s[i]),2);
-        else t.erase(t.find(s[i]),1);
+        int point=t.find(s[i]);
+        if(i==0){
+            t[point]=' ';
+            t.erase(point+1,1);
+            //t.erase(t.find(s[i]),2);
+        }
+        else{
+            t.erase(t.find(s[i]),1);
+        }
       }
     }
     if(ch==0) break;
   }
+  ts=t.size();
+  rep(i,ts){
+      if(t[i]==' '){t.erase(i,1); --i;}
+      //cout << i+1 << t << endl;
+  }
+  //cout << t << endl;
   if(t.empty()) cout << "YES\n";
   else cout << "NO\n";
   return 0;

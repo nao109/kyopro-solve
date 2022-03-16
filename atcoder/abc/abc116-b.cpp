ccd9b08a223f1688593d7eec6d82f55bc2b41692
nao109 <80878102+nao109@users.noreply.github.com>
2022-03-16 18:51:49 +0900

    work on abc116-b

---
+++ atcoder/abc/abc116-b.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+ll collatz(ll a){
+  if(a%2==0) return a/2;
+  else return a*3+1;
+}
+int main(){
+  cout << fixed << setprecision(7);
+  ll s; cin >> s;
+  vector<ll> a(1000000);
+  a[0]=s;
+  ll ans=0;
+  rep(i,1000000){
+    if(a[i]==4){
+      ans=i; break;
+    }
+    a[i+1]=collatz(a[i]);
+  }
+  cout << ans+4 << endl;
+  return 0;
+}

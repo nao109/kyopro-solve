0f0a4cfea729cae5941b92cf65be9860540a6457
nao109 <80878102+nao109@users.noreply.github.com>
2022-03-27 13:13:25 +0900

    solve abc244-a,b,c,d

---
+++ atcoder/abc/abc244-a.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+int main(){
+  cout << fixed << setprecision(7);
+  int n; string s; cin >> n >> s;
+  cout << s[n-1] << endl;
+  return 0; 
+}

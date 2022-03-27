24fd8673e254bb4f3b0a8cd022b7c6648bd5c22d
nao109 <haruki.2004.109@gmail.com>
2022-03-27 15:43:26 +0900

    solve abc084,106-b

--- atcoder/abc/abc084-b.cpp
+++ atcoder/abc/abc084-b.cpp
@@ -2,24 +2,29 @@
 using namespace std;
 using ll = long long;
 #define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
 int main(){
   cout << fixed << setprecision(7);
-  int a,b; string s;
-  cin >> a >> b >> s;
+  int a,b; string s; cin >> a >> b >> s;
   bool ans=true;
   rep(i,a+b+1){
-    if(i<a){
-      if('0'<=s[i]&&s[i]<='9') continue;
-      else{
+    if(0<=i&&i<a){
+      if(s[i]<'0'||'9'<s[i]){
+        //cerr << 1 << endl;
         ans=false; break;
       }
     }
     else if(i==a){
-      if(s[i]!='-') ans=false; break;
+      if(s[i]!='-'){
+        //cerr << 2 << endl;
+        ans=false; break;
+      }
     }
     else{
-      if('0'<=s[i]&&s[i]<='9') continue;
-      else{
+      if(s[i]<'0'||'9'<s[i]){
+        //cerr << 3 << endl;
         ans=false; break;
       }
     }

e1907419223e8170e2a7da6fbca781451106fcaa
nao109 <80878102+nao109@users.noreply.github.com>
2022-03-28 17:31:55 +0900

    solve abc221-b

--- atcoder/abc/abc221-b.cpp
+++ atcoder/abc/abc221-b.cpp
@@ -9,7 +9,8 @@ int main(){
   cout << fixed << setprecision(7);
   vector<string> s(1); string t; cin >> s[0] >> t;
   int ss=s[0].size(); rep(i,ss-1) s.pb(s[0]);
-  rep(i,ss-1) swap(s[i+1][i+1],s[i+1][i+2]);
+  rep(i,ss-1) swap(s[i][i],s[i][i+1]);
+  rep(i,ss) cerr << s[i] << endl;
   rep(i,ss){
     if(s[i]==t){
       cout << "Yes\n"; return 0;

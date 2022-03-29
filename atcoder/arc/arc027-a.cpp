cfa54f59c3b2e1af1c6a66767cdbf61fa3de0b5e
nao109 <haruki.2004.109@gmail.com>
2022-03-29 22:29:45 +0900

    solve abc057-b,161-b,185-b and arc027-a

---
+++ atcoder/arc/arc027-a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(7);
+  int h,m; cin >> h >> m;
+  m+=h*60;
+  cout << 18*60-m << endl;
+  return 0;
+}

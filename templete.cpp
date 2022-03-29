f0445b8b6f78bfeccda0840eb7194be1fa31a192
nao109 <haruki.2004.109@gmail.com>
2022-03-29 23:06:58 +0900

    add macro to templete.cpp

--- templete.cpp
+++ templete.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); i++)
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
 int main(){
   cout << fixed << setprecision(7);
   

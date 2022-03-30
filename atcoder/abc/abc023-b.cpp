ec80f16b3af0f778d1053dff6beb35d3a39460fc
nao109 <80878102+nao109@users.noreply.github.com>
2022-03-30 11:35:21 +0900

    work on abc023-b

---
+++ atcoder/abc/abc023-b.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(7);
+  int n; string s; cin >> n >> s;
+  string ch="b";
+  int cnt=1; bool ans=false;
+  while(ch.size()<s.size()){
+    if(cnt%3==1) ch="a"+ch+"c";
+    else if(cnt%3==2) ch="c"+ch+"a";
+    else ch="b"+ch+"b";
+    if(ch==s){ans=true; break;}
+    ++cnt;
+  }
+  if(ans) cout << cnt << endl;
+  else cout << "-1\n";
+  return 0;
+}

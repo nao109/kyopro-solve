3d5cac83eae463453be686f272932fda5a3f386f
nao109 <haruki.2004.109@gmail.com>
2022-03-30 12:27:17 +0900

    solve abc023,042-b

--- atcoder/abc/abc023-b.cpp
+++ atcoder/abc/abc023-b.cpp
@@ -9,7 +9,9 @@ int main(){
   cout << fixed << setprecision(7);
   int n; string s; cin >> n >> s;
   string ch="b";
-  int cnt=1; bool ans=false;
+  int cnt=0; bool ans=false;
+  if(s=="b") ans=true;
+  else ++cnt;
   while(ch.size()<s.size()){
     if(cnt%3==1) ch="a"+ch+"c";
     else if(cnt%3==2) ch="c"+ch+"a";

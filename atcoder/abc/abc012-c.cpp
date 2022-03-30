cf2291111f2fac1401f07cf0d7c380731d4830cf
nao109 <haruki.2004.109@gmail.com>
2022-03-30 12:29:02 +0900

    solve abc012-c

--- atcoder/abc/abc012-c.cpp
+++ atcoder/abc/abc012-c.cpp
@@ -10,8 +10,8 @@ int main(){
   int n; cin >> n;
   int diff=2025-n;
   queue<int> ch;
-  rep(i,10){
-    if(diff%(i+1)==0&&diff/(i+1)<10) ch.push(i+1);
+  for(int i=1; i<10; ++i){
+    if(diff%i==0&&diff/i<10) ch.push(i);
   }
   while(!ch.empty()){
     cout << ch.front() << " x " << diff/ch.front() << endl;

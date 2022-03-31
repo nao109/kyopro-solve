eccd1a7628eba4307a93aa754c34ff3f059a84ae
nao109 <80878102+nao109@users.noreply.github.com>
2022-03-31 17:34:02 +0900

    rework on abc225-c

--- atcoder/abc/abc225-c.cpp
+++ atcoder/abc/abc225-c.cpp
@@ -10,15 +10,13 @@ int main(){
   int n,m; cin >> n >> m;
   int b[n][m]; rep(i,n)rep(j,m) cin >> b[i][j];
   bool ans=true;
-  rep(i,n){
-    rep(j,m){
-      if(i>0&&j==0){
-        if(b[i][0]!=b[i-1][0]+7){cout << "No\n"; return 0;}
-      }
-      else if(j>0){
-        if(b[i][j]!=b[i][j-1]+1){cout << "No\n"; return 0;}
-      }
+  rep(i,n)rep(j,m){
+    if(j<m-1){
+      if(b[0][j+1]!=b[0][j]+1){cout << "No\n"; return 0;}
     }
+  }
+  rep(i,n-1){
+    if(b[i+1][0]!=b[i][0]+7){cout << "No\n"; return 0;}
   }
   cout << "Yes\n";
   return 0;

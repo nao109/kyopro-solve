433bf1c8330a86b82aeffeef5879708b087f8e2e
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-05 16:06:23 +0900

    solve abc225-c

--- atcoder/abc/abc225-c.cpp
+++ atcoder/abc/abc225-c.cpp
@@ -9,23 +9,27 @@ int main(){
   cout << fixed << setprecision(7);
   int n,m; cin >> n >> m;
   int b[n][m]; rep(i,n)rep(j,m) cin >> b[i][j];
-
+  bool flag=true;
   rep(i,n)rep(j,m){
     if(j<m-1){
-      if(b[0][j+1]!=b[0][j]+1){cout << "No\n"; return 0;}
+      if(b[0][j+1]!=b[0][j]+1) flag=false;
     }
   }
   
-  int l=b[0][0]%7;
+  /*int l=b[0][0]%7;
   if(l==0){cout << "No\n"; return 0;}
   rep(i,7){
     if(n==i+1&&l>7-i){cout << "No\n"; return 0;}
-  }
+  }*/
+  int l=(b[0][0]-1)%7+1,r=(b[0][m-1]-1)%7+1;
+  cerr << l << " " << r << endl;
+  if(l<1||r>7||r<l) flag=false;
 
   rep(i,n-1){
-    if(b[i+1][0]!=b[i][0]+7){cout << "No\n"; return 0;}
+    if(b[i+1][0]!=b[i][0]+7) flag=false;
   }
 
-  cout << "Yes\n";
+  if(flag) cout << "Yes\n";
+  else cout << "No\n";
   return 0;
 }

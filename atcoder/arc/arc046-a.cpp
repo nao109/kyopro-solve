b6ee8e3edd4b65d5f2b7b469d53450d1c8f00082
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-09 18:30:25 +0900

    work on arc046-a

---
+++ atcoder/arc/arc046-a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(15);
+  int n; cin >> n;
+  int ch1=(n+8)/9,ch2=n%9;
+  rep(i,ch1) cout << ch2;
+  cout << endl;
+  return 0;
+}

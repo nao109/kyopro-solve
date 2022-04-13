63db0468307ca925b45de3d8b626d4484364e634
nao109 <haruki.2004.109@gmail.com>
2022-04-13 23:47:29 +0900

    solve abc196-c

---
+++ atcoder/abc/abc196-c.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(15);
+  ll n; cin >> n;
+  ll cnt=0;
+  for(ll i=1; i<=1000000; ++i){
+    if(stoll(to_string(i)+to_string(i))>n) break;
+    ++cnt;
+  }
+  cout << cnt << endl;
+  return 0;
+}

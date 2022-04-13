b1543fc0bdc4f7e02c010d1f6699b08db082dca6
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-13 22:54:48 +0900

    solve abc182-b

--- atcoder/abc/abc182-b.cpp
+++ atcoder/abc/abc182-b.cpp
@@ -9,14 +9,14 @@ int main(){
   cout << fixed << setprecision(15);
   int n; cin >> n;
   vector<int> a(n); rep(i,n) cin >> a[i];
-  vector<pair<int,int>> cnt(n-1);
-  rep(i,n-1){cnt[i].fi=0; cnt[i].se=i+2;}
-  for(int i=2; i<=n; i++){
+  vector<pair<int,int>> cnt(999);
+  rep(i,999){cnt[i].fi=0; cnt[i].se=i+2;}
+  for(int i=2; i<=1000; ++i){
     rep(j,n){
       if(a[j]%i==0) ++cnt[i-2].fi;
     }
   }
   sort(cnt.begin(),cnt.end());
-  cout << cnt[n-2].se << endl;
+  cout << cnt[998].se << endl;
   return 0;
 }

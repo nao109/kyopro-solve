1eb15d264df1aeee13e881e9fc9f9d2d55b7ff52
nao109 <haruki.2004.109@gmail.com>
2022-04-15 19:02:52 +0900

    rework on abc245-c 4

--- atcoder/abc/abc245-c.cpp
+++ atcoder/abc/abc245-c.cpp
@@ -12,13 +12,16 @@ int main(){
   rep(i,n) cin >> a[i]; rep(i,n) cin >> b[i];
   bool ch[2][n]; ch[0][0]=true; ch[1][0]=true;
   rep(i,n-1){
+    ch[0][i+1]=false; ch[1][i+1]=false;
     if(ch[0][i]){
-      if(abs(a[i+1]-a[i])<=k||abs(b[i+1]-a[i])<=k) ch[0][i+1]=true;
+      if(abs(a[i+1]-a[i])<=k) ch[0][i+1]=true;
+      if(abs(b[i+1]-a[i])<=k) ch[1][i+1]=true;
     }
-    else if(ch[1][i]){
-      if(abs(a[i+1]-b[i])<=k||abs(b[i+1]-b[i])<=k) ch[1][i+1]=true;
+    if(ch[1][i]){
+      if(abs(a[i+1]-b[i])<=k) ch[0][i+1]=true;
+      if(abs(b[i+1]-b[i])<=k) ch[1][i+1]=true;
     }
-    else ch[0][i+1]=false; ch[0][i+1]=false;
+    cerr << ch[0][i+1] << " " << ch[1][i+1] << endl;
   }
   if(ch[0][n-1]||ch[1][n-1]) cout << "Yes\n";
   else cout << "No\n";

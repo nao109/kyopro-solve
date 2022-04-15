930dcd959eb5631513c992c554d4cc2d48812fbd
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-15 19:00:54 +0900

    rework on abc245-c

--- atcoder/abc/abc245-c.cpp
+++ atcoder/abc/abc245-c.cpp
@@ -6,18 +6,18 @@ using ll = long long;
 #define se second
 #define pb push_back
 int main(){
-  cout << fixed << setprecision(7);
+  cout << fixed << setprecision(15);
   int n,k; cin >> n >> k;
   vector<int> a(n),b(n);
   rep(i,n) cin >> a[i]; rep(i,n) cin >> b[i];
-  bool ans=true;
-  rep(i,n){
-    vector<int> diff({a[i]-a[i+1],a[i]-b[i+1],b[i]-a[i+1],b[i]-b[i+1]});
-    rep(j,4){
-      if(abs(diff[j])>k){ans=false; break;}
+  vector<bool> ch(n,false); ch[0]=true;
+  rep(i,n-1){
+    if(ch[i]){
+      if(abs(a[i+1]-a[i])<=k||abs(b[i+1]-a[i])<=k) ch[i+1]=true;
+      else if(abs(a[i+1]-b[i])<=k||abs(b[i+1]-b[i])<=k) ch[i+1]=true;
     }
   }
-  if(ans) cout << "Yes\n";
+  if(ch[n-1]) cout << "Yes\n";
   else cout << "No\n";
   return 0;
 }

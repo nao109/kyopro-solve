6b001ba39770c5699093a84bbb521143da5b7804
nao109 <haruki.2004.109@gmail.com>
2022-04-16 18:41:45 +0900

    work on abc219-c 2

--- atcoder/abc/abc219-c.cpp
+++ atcoder/abc/abc219-c.cpp
@@ -7,19 +7,40 @@ using ll = long long;
 #define pb push_back
 int main(){
   cout << fixed << setprecision(15);
-  string x; int n; cin >> x >> n;
+
+  vector<pair<char,char>> ch(26);
+  rep(i,26){ch[i].fi='a'+i; cin >> ch[i].se;}
+
+  int n; cin >> n;
   vector<string> s(n); rep(i,n) cin >> s[i];
+  
   rep(i,n){
     int ss=s[i].size();
     rep(j,ss){
-      s[i][j]=x[s[i][j]-'a'];
+      s[i][j]=ch[s[i][j]-'a'].se;
     }
   }
   sort(s.begin(),s.end());
+  
+  rep(i,52){
+    if(i<26) cerr << ch[i].fi;
+    else if(i<52) cerr << ch[i].se;
+    if((i+1)%26==0) cerr << endl;
+  }
+
+  rep(i,26) swap(ch[i].fi,ch[i].se);
+  sort(ch.begin(),ch.end());
+  
+  rep(i,52){
+    if(i<26) cerr << ch[i].fi;
+    else if(i<52) cerr << ch[i].se;
+    if((i+1)%26==0) cerr << endl;
+  }
+  
   rep(i,n){
     int ss=s[i].size();
     rep(j,ss){
-      s[i][j]=x[s[i][j]-'a'];
+      s[i][j]=ch[s[i][j]-'a'].se;
     }
   }
   rep(i,n) cout << s[i] << endl;

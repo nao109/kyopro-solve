96a0bb11b358c8808fb23531b90473c626f9d272
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-17 18:22:43 +0900

    solve arc017-a and code_festival_2014_final-a,b

---
+++ atcoder/other/code_festival_2014_final-a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(15);
+  double s; cin >> s;
+  double time=50/s;
+  cout << time << endl;
+  return 0;
+}

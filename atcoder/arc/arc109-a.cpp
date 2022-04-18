4563bee676941ded835be527e6ebcf88b3e57662
nao109 <haruki.2004.109@gmail.com>
2022-04-18 11:49:11 +0900

    solve arc109-a

--- atcoder/arc/arc109-a.cpp
+++ atcoder/arc/arc109-a.cpp
@@ -9,8 +9,8 @@ int main(){
   cout << fixed << setprecision(15);
   int a,b,x,y; cin >> a >> b >> x >> y;
   int time=y*(abs(a-b))+x;
-  if(a-b==1) time=x;
-  else if(2*x<y) time=2*x*abs(a-b)+x;
+  if(a<=b) time=min(2*x,y)*abs(a-b)+x;
+  else time=min(2*x,y)*abs(a-b-1)+x;
   cout << time << endl;
   return 0;
 }

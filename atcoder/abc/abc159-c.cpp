cea0667dca1cb26a029198ebb8876ce21e3dfd1c
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-18 11:16:19 +0900

    solve abc159-c

---
+++ atcoder/abc/abc159-c.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(15);
+  long double l; cin >> l;
+  long double ans=l*l*l/27;
+  cout << ans << endl;
+  return 0;
+}

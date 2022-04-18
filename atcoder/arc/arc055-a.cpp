9925090db872188c05a3d7a54d3053f3f976e7f1
nao109 <haruki.2004.109@gmail.com>
2022-04-18 22:25:41 +0900

    solve arc055-a

---
+++ atcoder/arc/arc055-a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(15);
+  int n; cin >> n;
+  cout << 1;
+  rep(i,n-1) cout << 0;
+  cout << "7\n";
+  return 0;
+}

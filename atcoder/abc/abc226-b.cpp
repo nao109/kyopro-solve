a6fc3c5ca54dfa6a391edde1224b5cf60bb631ea
nao109 <haruki.2004.109@gmail.com>
2022-04-20 19:36:45 +0900

    work on abc226-b

---
+++ atcoder/abc/abc226-b.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+int main(){
+  cout << fixed << setprecision(15);
+  int n; cin >> n;
+  vector<string> s(n,"");
+  rep(i,n){
+    int j; cin >> j;
+    rep(i,j){string t; cin >> t; s[i]+=t;}
+  }
+  sort(s.begin(),s.end());
+  set<string> cnt;
+  rep(i,n){
+    if(!cnt.count(s[i])) cnt.insert(s[i]);
+  }
+  int ans=cnt.size();
+  cout << ans << endl;
+  return 0;
+}

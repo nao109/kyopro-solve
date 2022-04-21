5be8d4af309510a059e6a16318f85ed52e1d6d54
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-21 18:54:39 +0900

    solve abc220-c

--- atcoder/abc/abc220-c.cpp
+++ atcoder/abc/abc220-c.cpp
@@ -7,14 +7,14 @@ using ll = long long;
 #define pb push_back
 int main(){
   cout << fixed << setprecision(15);
-  int n; cin >> n;
-  vector<int> a(n); int suma=0;
-  rep(i,n){
+  ll n; cin >> n;
+  vector<ll> a(n); ll suma=0;
+  for(ll i=0; i<n; ++i){
     cin >> a[i];
     suma+=a[i];
   }
-  int x; cin >> x;
-  int cnt=(x/suma)*a.size(),i=0;
+  ll x; cin >> x;
+  ll cnt=(x/suma)*n,i=0;
   x-=(x/suma)*suma;
   while(x>=0){
     x-=a[i]; ++cnt; ++i;

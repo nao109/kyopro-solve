06da2cc42da4f7ca67dc1ec5c0257f12020aff61
nao109 <80878102+nao109@users.noreply.github.com>
2022-04-25 12:31:41 +0900

    work on pakencamp_2021_day2-d 3

--- atcoder/other/pakencamp_2021_day2-d.cpp
+++ atcoder/other/pakencamp_2021_day2-d.cpp
@@ -9,17 +9,13 @@ using ll = long long;
 int main(){
   cout << fixed << setprecision(15);
   int n,m; cin >> n >> m;
-  vector<int> a(m);
-  rep(i,m) a[i]=0;
+  vector<int> cnt(m,0);
   rep(i,n){
-    int c; cin >> c; ++a[c-1];
+    int a; cin >> a; ++cnt[a-1];
   }
-  int mi=3000,Ma=0;
-  rep(i,m){
-    if(mi>a[i]) mi=a[i];
-    if(Ma<a[i]) Ma=a[i];
-  }
-  rep(i,m) cerr << a[i] << endl;
-  cout << mi << " " << Ma << endl;
+  int mi=200000,Ma=0;
+  //rep(i,m) cerr << cnt[i] << endl;
+  sort(all(cnt));
+  cout << cnt[0] << " " << cnt[m-1] << endl;
   return 0;
 }

3983f8042541ab0be7701c57898d8cef25d6cd05
nao109 <haruki.2004.109@gmail.com>
2022-04-28 00:14:19 +0900

    solve abc219-c

--- atcoder/abc/abc219-c.cpp
+++ atcoder/abc/abc219-c.cpp
@@ -6,27 +6,27 @@ using ll = long long;
 #define se second
 #define pb push_back
 #define all(a) a.begin(),a.end()
-bool f(vector<string> &s,vector<pair<char,char>> &x){
-  int ss=s.size();
-  rep(i,ss){
-    int strs=s[i].size();
-    rep(j,strs) s[i][j]=x[s[i][j]-'a'].se;
-  }
-}
 int main(){
   cout << fixed << setprecision(15);
-  vector<pair<char,char>> x(26); rep(i,26){x[i].se='a'+i; cin >> x[i].fi;}
+  vector<pair<char,char>> x(26);
+  rep(i,26){x[i].se='a'+i; cin >> x[i].fi;}
   sort(all(x));
   int n; cin >> n;
   vector<string> s(n);
-  rep(i,n) cin >> s[i];
-  f(s,x);
+  rep(i,n){
+    cin >> s[i];
+    int ss=s[i].size();
+    rep(j,ss) s[i][j]=x[s[i][j]-'a'].se;
+  }
   sort(all(s));
   //rep(i,n) cerr << s[i] << endl;
   rep(i,26) swap(x[i].fi,x[i].se);
   sort(all(x));
   //rep(i,26) cerr << x[i].fi << " " << x[i].se << endl;
-  f(s,x);
+  rep(i,n){
+    int ss=s[i].size();
+    rep(j,ss) s[i][j]=x[s[i][j]-'a'].se;
+  }
   rep(i,n) cout << s[i] << endl;
   return 0;
 }

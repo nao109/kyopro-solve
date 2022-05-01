7011bedfc5ff9182ef0dcdd6e6797be77eb34bd3
nao109 <haruki.2004.109@gmail.com>
2022-05-01 15:39:06 +0900

    solve abc075-b

---
+++ atcoder/abc/abc075-b.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(),a.end()
+int main(){
+  cout << fixed << setprecision(15);
+  int dx[8]={-1,-1,-1,0,0,1,1,1},dy[8]={-1,0,1,-1,1,-1,0,1};
+  int h,w; cin >> h >> w;
+  vector<string> s(h); rep(i,h) cin >> s[i];
+  rep(i,h)rep(j,w){}
+  return 0;
+}

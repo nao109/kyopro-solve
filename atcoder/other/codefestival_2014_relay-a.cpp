4ae583faf8c96b8648dcd497881ccfbd3aca2c26
nao109 <80878102+nao109@users.noreply.github.com>
2022-05-05 15:00:07 +0900

    solve codefestival_2014_relay-a,b

---
+++ atcoder/other/codefestival_2014_relay-a.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define pi acos(-1)
+int main(){
+  cout << fixed << setprecision(15);
+  int n; cin >> n;
+  if(n==1) cout << 0;
+  else cout << 1;
+  cout << endl;
+  return 0;
+}

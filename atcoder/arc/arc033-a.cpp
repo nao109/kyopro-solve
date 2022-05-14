90239837ccce8e147c9550c3e7b1a8a8a5f528a9
nao109 <haruki.2004.109@gmail.com>
2022-05-14 15:51:59 +0900

    solve abc109-c,arc033-a

---
+++ atcoder/arc/arc033-a.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+#define pi acos(-1)
+int main(){
+  int n;
+  cin >> n;
+  cout << n*(n+1)/2 << endl;
+  return 0;
+}

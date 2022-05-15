871dbecb4d0999001b9274f2d1dc5e999b588bdb
nao109 <80878102+nao109@users.noreply.github.com>
2022-05-15 10:00:50 +0900

    work on abc251-d

---
+++ atcoder/abc/abc251-d.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+#define pi acos(-1)
+int main(){
+  for(int i=1; i<100; ++i) cout << i <<  " ";
+  for(int i=1; i<100; ++i) cout << i*100 <<  " ";
+  for(int i=1; i<100; ++i) cout << i*10000 <<  " ";
+  cout << endl;
+  return 0;
+}

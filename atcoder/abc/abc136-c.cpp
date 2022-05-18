649669184f7390f0b7cacf7168a9a1aec058e018
nao109 <80878102+nao109@users.noreply.github.com>
2022-05-18 13:59:42 +0900

    solve abc136-c

--- atcoder/abc/abc136-c.cpp
+++ atcoder/abc/abc136-c.cpp
@@ -1,27 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); ++i)
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+#define pi acos(-1)
 int main(){
-  cout << fixed << setprecision(7);
-  int n; cin >> n;
-  vector<int> h(n); rep(i,n) cin >> h[i];
-  bool ch=true;
-  rep(i,n-1){
-    if(h[i]-h[i+1]>1){
-      ch=false; break;
+  int n;
+  cin >> n;
+  vector<int> h(n);
+  for(int i=0; i<n; ++i) cin >> h[i];
+  reverse(all(h));
+  for(int i=0; i<n-1; ++i){
+    if(h[i]+2<=h[i+1]){
+      cout << "No\n";
+      return 0;
     }
-    else if(h[i]-h[i+1]==1) --h[i];
+    else if(h[i]+1==h[i+1]) --h[i+1];
   }
-  if(ch){
-    rep(i,n-1){
-      if(h[i]-h[i+1]<=0) continue;
-      else if(h[i]-h[i+1]>=1){
-        ch=false; break;
-      }
-    }
-  }
-  if(ch) cout << "Yes\n";
-  else cout << "No\n";
+  cout << "Yes\n";
   return 0;
 }

ddfef9eff55abf745fe322698e410dcda543b7f0
nao109 <80878102+nao109@users.noreply.github.com>
2022-05-27 12:34:15 +0900

    solve arc050-a

---
+++ atcoder/arc/arc050-a.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+#define pi acos(-1)
+int main(){
+  char C,c;
+  cin >> C >> c;
+  cout << (C-c=='A'-'a' ? "Yes\n" : "No\n");
+  return 0;
+}

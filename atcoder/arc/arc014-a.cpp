9b12927d20bc5eaba9d70df0951d29cf38e01b0e
nao109 <haruki.2004.109@gmail.com>
2022-05-28 17:16:46 +0900

    solve arc014-a

---
+++ atcoder/arc/arc014-a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int n;
+  cin >> n;
+  cout << (n%2==0 ? "Blue\n" : "Red\n");
+  return 0;
+}

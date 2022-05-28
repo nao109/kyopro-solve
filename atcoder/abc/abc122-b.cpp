32fa33c12769499d02623e6d3956aa4b36c102f8
nao109 <haruki.2004.109@gmail.com>
2022-05-28 13:38:40 +0900

    solve abc122-b

--- atcoder/abc/abc122-b.cpp
+++ atcoder/abc/abc122-b.cpp
@@ -4,18 +4,18 @@ using ll = long long;
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
-#define pi acos(-1)
 int main(){
   string s; cin >> s;
   int n=s.size();
   int ma=0,cnt=0;
   for(int i=0; i<n; ++i){
+    cerr << cnt << endl;
     if(s[i]=='A' || s[i]=='C' || s[i]=='G' || s[i]=='T') ++cnt;
     else{
       ma=max(cnt,ma);
       cnt=0;
     }
   }
-  cout << ma << endl;
+  cout << max(cnt,ma) << endl;
   return 0;
 }

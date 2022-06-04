a18d43a68c31c0c3358aaf7ac65e3a6aeb412b5b
nao109 <80878102+nao109@users.noreply.github.com>
2022-06-04 23:11:05 +0900

    solve abc254-a,b and work on abc254-c during the contest

---
+++ atcoder/abc/abc254-a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  string n;
+  cin >> n;
+  cout << n[1] << n[2] << endl;
+  return 0;
+}

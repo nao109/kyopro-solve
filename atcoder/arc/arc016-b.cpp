55aa87567bebd7ed5ef724794805f3f013787d80
nao109 <haruki.2004.109@gmail.com>
2022-06-04 17:24:26 +0900

    work on arc016-b

---
+++ atcoder/arc/arc016-b.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int n;
+  cin >> n;
+  vector<string> s(n);
+  for(int i=0; i<n; ++i) cin >> s[i];
+  int ans=0;
+  for(int i=0; i<9; ++i){
+    bool longs=false;
+    for(int j=0; j<n; ++j){
+      if(s[j][i]=='x') ++ans;
+      if(s[j][i]=='o'){
+        if(!longs){
+          longs=true;
+          ++ans;
+        }
+      }
+      if(longs && s[j][i]=='x') longs=false;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}

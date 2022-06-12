f325f73fd4ec41d3f41ec1fb57afa415c50da124
nao109 <80878102+nao109@users.noreply.github.com>
2022-06-13 08:51:54 +0900

    solve abc176-c

--- atcoder/abc/abc176-c.cpp
+++ atcoder/abc/abc176-c.cpp
@@ -4,15 +4,17 @@ using ll = long long;
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
-#define pi acos(-1)
 int main(){
   int n;
   cin >> n;
   vector<int> a(n);
   for(int i=0; i<n; ++i) cin >> a[i];
-  int sum=0;
+  ll sum=0;
   for(int i=0; i<n-1; ++i){
-    if(a[i]>a[i+1]){sum+=a[i]-a[i+1]; a[i+1]=a[i];}
+    if(a[i]>a[i+1]){
+      sum+=a[i]-a[i+1];
+      a[i+1]=a[i];
+    }
   }
   cout << sum << endl;
   return 0;

ec7ec7b70493b9731a15ad66575322081a655cbd
nao109 <80878102+nao109@users.noreply.github.com>
2022-06-16 20:57:56 +0900

    solve abc198-c with editorial

--- atcoder/abc/abc198-c.cpp
+++ atcoder/abc/abc198-c.cpp
@@ -4,12 +4,11 @@ using ll = long long;
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
-#define pi acos(-1)
 int main(){
   double r,x,y;
   cin >> r >> x >> y;
   double l=sqrt(x*x+y*y);
-  double ans=ceil(l/r);
-  cout << ans << endl;
+  if(l!=r && l<=2*r) cout << 2 << endl;
+  else cout << ceil(l/r) << endl;
   return 0;
 }

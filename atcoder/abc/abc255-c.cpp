39be6f2d3c15990901ddbf300f4a552855ab4579
nao109 <80878102+nao109@users.noreply.github.com>
2022-06-18 14:48:07 +0900

    solve abc255-c with editorial

--- atcoder/abc/abc255-c.cpp
+++ atcoder/abc/abc255-c.cpp
@@ -7,24 +7,18 @@ using ll = long long;
 int main(){
   ll x,a,d,n;
   cin >> x >> a >> d >> n;
-  if(d>0){
+  if(d<0){
+    a=a+d*(n-1);
+    d*=-1;
+  }  
+  if(d==0) cout << abs(x-a) << endl;
+  else{
     if(x<=a) cout << a-x << endl;
     else if(a+d*(n-1)<=x) cout << x-(a+d*(n-1)) << endl;
     else{
       ll sub=(x-a)%d;
       cout << min(sub,d-sub) << endl;
     }
-  }
-  else if(d<0){
-    if(x>=a) cout << x-a << endl;
-    else if(a+d*(n-1)>=x) cout << (a+d*(n-1))-x << endl;
-    else{
-      ll sub=(a-x)%d;
-      cout << min(sub,d-sub) << endl;
-    }
-  }
-  else{
-    cout << abs(x-a) << endl;
   }
   return 0;
 }

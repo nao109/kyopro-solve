d507cb3ab12c02b07c6b8c6f021175038ae08aee
nao109 <haruki.2004.109@gmail.com>
2022-07-01 00:39:59 +0900

    work on abc202-c

--- atcoder/abc/abc202-c.cpp
+++ atcoder/abc/abc202-c.cpp
@@ -8,18 +8,16 @@ int main(){
   int n;
   cin >> n;
   vector<int> a(n),b(n),c(n);
-  for(int i=0; i<n; ++i) cin >> a[i];
-  for(int i=0; i<n; ++i) cin >> b[i];
-  for(int i=0; i<n; ++i) cin >> c[i];
-  map<int,int> ch;
-  for(int i=0; i<n; ++i){
-    if(ch.count(b[c[i]-1])) ++ch[b[c[i]-1]];
-    else ch[b[c[i]-1]]=1;
+  for(int &i:a) cin >> i;
+  for(int &i:b) cin >> i;
+  for(int &i:c){
+    cin >> i;
+    --i;
   }
+  vector<int> ch(n);
+  for(int i=0; i<n; ++i) ++ch[b[c[i]]];
   int ans=0;
-  for(int i=0; i<n; ++i){
-    if(ch.count(a[i])) ans+=ch[a[i]];
-  }
+  for(int i=0; i<n; ++i) ans+=ch[a[i]];
   cout << ans << endl;
   return 0;
 }

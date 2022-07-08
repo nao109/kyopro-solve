0ea3d7ce47c3e077043cdac239d6f495e2eb5f4c
nao109 <80878102+nao109@users.noreply.github.com>
2022-07-08 20:25:27 +0900

    solve abc154-d with editorial

--- atcoder/abc/abc154-d.cpp
+++ atcoder/abc/abc154-d.cpp
@@ -8,17 +8,15 @@ int main(){
   int n,k;
   cin >> n >> k;
   vector<double> p(n);
-  for(auto &i:p){
-    cin >> i;
-    i=(i+1)/2;
+  for(int i=0; i<n; ++i){
+    cin >> p[i];
+    p[i]=(p[i]+1)/2;
   }
   double ma=0,sum=0;
   for(int i=0; i<n; ++i){
     sum+=p[i];
-    if(i>=k){
-      sum-=p[i-k];
-      ma=max(sum,ma);
-    }
+    if(i>=k) sum-=p[i-k];
+    ma=max(sum,ma);
   }
   cout << fixed << setprecision(7) << ma << endl;
   return 0;

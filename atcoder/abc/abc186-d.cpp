0d5be57339e67491914df5e6eb68a3942ee32334
nao109 <80878102+nao109@users.noreply.github.com>
2022-07-14 16:22:00 +0900

    work on abc186-d

---
+++ atcoder/abc/abc186-d.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  ll n;
+  cin >> n;
+  vector<ll> a(n);
+  for(ll &i:a) cin >> i;
+  sort(all(a),greater<int>());
+  ll ans=0,sum=0;
+  for(int i=1; i<n; ++i) sum+=a[i];
+  for(int i=0; i<n-1; ++i){
+    ans+=(n-i-1)*a[i]-sum;
+    sum-=a[i+1];
+  }
+  cout << ans << endl;
+  return 0;
+}

98e739cdfa41b0f1cd8feffff3851be95a2001e6
nao109 <haruki.2004.109@gmail.com>
2022-07-19 13:23:41 +0900

    solve arc116-a with editorial

--- atcoder/arc/arc116-a.cpp
+++ atcoder/arc/arc116-a.cpp
@@ -4,27 +4,14 @@ using ll = long long;
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
-string solve(){
-  ll n;
-  cin >> n;
-  vector<ll> cnt(2,0);
-  for(ll i=1; i*i<=n; ++i){
-    if(n%i==0){
-      if(i%2==0) ++cnt[0];
-      else ++cnt[1];
-      if(i*i<n){
-        if((n/i)%2==0) ++cnt[0];
-        else ++cnt[1];
-      }
-    }
-  }
-  if(cnt[0]==cnt[1]) return "Same";
-  else if(cnt[0]>cnt[1]) return "Even";
-  else return "Odd";
-}
 int main(){
-  int t;
+  ll t,n;
   cin >> t;
-  for(int i=0; i<t; ++i) cout << solve() << endl;
+  for(int i=0; i<t; ++i){
+    cin >> n;
+    if(n%4==0) cout << "Even\n";
+    else if(n%4==2) cout << "Same\n";
+    else cout << "Odd\n";
+  }
   return 0;
 }

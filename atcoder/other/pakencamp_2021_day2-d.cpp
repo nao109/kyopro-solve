c48d1362776d09a170aec6bdd3409423de18191c
nao109 <80878102+nao109@users.noreply.github.com>
2022-07-22 01:43:48 +0900

    work on pakencamp_2021_day2-d

--- atcoder/other/pakencamp_2021_day2-d.cpp
+++ atcoder/other/pakencamp_2021_day2-d.cpp
@@ -1,19 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); ++i)
 #define fi first
 #define se second
-#define pb push_back
 #define all(a) a.begin(),a.end()
 int main(){
-  cout << fixed << setprecision(15);
-  int n,m; cin >> n >> m;
+  int n,m;
+  cin >> n >> m;
   vector<int> cnt(m,0);
-  rep(i,n){
-    int a; cin >> a; ++cnt.at(a-1);
+  cerr << cnt.capacity() << endl;
+  for(int i=0; i<n; ++i){
+    int a;
+    cin >> a;
+    ++cnt[a-1];
   }
   sort(all(cnt));
-  cout << cnt.at(0) << " " << cnt.at(m-1) << endl;
+  cout << cnt[0] << endl;
+  cout << cnt[m-1] << endl;
   return 0;
 }

d7f9116ee5ad333472f6c095f6d86ab979cfc462
nao109 <80878102+nao109@users.noreply.github.com>
2022-07-23 00:01:08 +0900

    work on agc010-a

---
+++ atcoder/agc/agc010-a.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int n;
+  cin >> n;
+  int even=0,odd=0;
+  for(int i=0; i<n; ++i){
+    int a;
+    cin >> a;
+    if(a%2==1) ++odd;
+    else ++even;
+  }
+
+  odd=odd%2;
+  even=min(1,even);
+
+  if(even+odd==1) cout << "YES\n";
+  else cout << "NO\n";
+  return 0;
+}

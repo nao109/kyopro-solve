5006502198784752a714e241c5db0defa53e1d02
nao109 <80878102+nao109@users.noreply.github.com>
2022-08-05 15:52:49 +0900

    work on typical90-g

---
+++ atcoder/other/typical90/typical90-g.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for(int &i:a) cin >> i;
+  sort(all(a));
+  int q;
+  cin >> q;
+  for(int i=0; i<q; ++i){
+    int b;
+    cin >> b;
+    auto iter=lower_bound(all(a), b);
+    auto pos=iter-a.begin();
+    if(pos==0) cout << a[pos]-b << endl;
+    else cout << min(b-a[pos-1],a[pos]-b) << endl;
+  }
+  return 0;
+}

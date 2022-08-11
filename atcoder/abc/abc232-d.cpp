f95285f8158fe4539be80f957fcb38909609874d
nao109 <80878102+nao109@users.noreply.github.com>
2022-08-11 21:21:48 +0900

    solve abc232-d

--- atcoder/abc/abc232-d.cpp
+++ atcoder/abc/abc232-d.cpp
@@ -4,6 +4,7 @@ using ll = long long;
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
+const int inf = 1000000000;
 int main(){
   int h,w;
   cin >> h >> w;
@@ -14,7 +15,7 @@ int main(){
   for(int i=0; i<h; ++i){
     for(int j=0; j<w; ++j){
       if(i==0 && j==0) continue;
-      if(c[i][j]=='#') dp[i][j]=0;
+      if(c[i][j]=='#') dp[i][j]=-inf;
       else if(i==0) dp[i][j]=dp[i][j-1]+1;
       else if(j==0) dp[i][j]=dp[i-1][j]+1;
       else dp[i][j]=max(dp[i-1][j],dp[i][j-1])+1;

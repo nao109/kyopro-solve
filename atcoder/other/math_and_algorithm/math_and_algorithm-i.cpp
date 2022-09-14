ab0f91733149de1b4dd5fa3870b5660faf11b89a
nao109 <80878102+nao109@users.noreply.github.com>
2022-09-14 19:53:45 +0900

    rework on math_and_algorithm-i 2

--- atcoder/other/math_and_algorithm/math_and_algorithm-i.cpp
+++ atcoder/other/math_and_algorithm/math_and_algorithm-i.cpp
@@ -9,16 +9,16 @@ int main(){
   cin >> n >> s;
   vector<ll> a(n+1);
   for(ll i=1; i<=n; ++i) cin >> a[i];
-  
-  vector<vector<bool>> dp(n+1,vector<bool>(10001));
+  vector<vector<bool>> dp(n+1,vector<bool>(600001));
   dp[0][0]=true;
+  
   for(ll i=1; i<=n; ++i){
-    for(ll j=1; j<=10000; ++j){
+    for(ll j=0; j<=600001; ++j){
       dp[i][j]=dp[i-1][j];
-      if(j>=a[i]) dp[i][j]=(dp[i-1][j] || dp[i-1][j-a[i]]);
+      if(j>=a[i]) dp[i][j]=dp[i-1][j-a[i]];
     }
   }
-
+  
   cout << (dp[n][s] ? "Yes\n" : "No\n");
   return 0;
 }

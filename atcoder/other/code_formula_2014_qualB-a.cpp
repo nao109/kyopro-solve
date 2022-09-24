856ff4d13b5cb8464f42465d689b414bc4b9c1b8
nao109 <80878102+nao109@users.noreply.github.com>
2022-09-24 10:33:06 +0900

    solve code_formula_2014_qualB-a

---
+++ atcoder/other/code_formula_2014_qualB-a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int a;
+  cin >> a;
+  cout << 7-a << endl;
+  return 0;
+}

b9f978b8d404ab5d6b6f071ea3a1863211b65786
nao109 <haruki.2004.109@gmail.com>
2022-09-24 10:33:31 +0900

    solve codefestival_2015_qualA,B-a

---
+++ atcoder/other/codefestival_2015_qualA-a.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  string s;
+  cin >> s;
+  s.back()='5';
+  cout << s << endl;
+  return 0;
+}

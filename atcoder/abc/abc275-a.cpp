786a62189f44c0318b84dc41db0b90d77c83823f
nao109 <haruki.2004.109@gmail.com>
2022-11-08 13:28:04 +0900

    update abc275-a

--- atcoder/abc/abc275-a.cpp
+++ atcoder/abc/abc275-a.cpp
@@ -5,6 +5,14 @@ using ll = long long;
 #define se second
 #define all(a) a.begin(),a.end()
 int main(){
-  
+  int n;
+  cin >> n;
+  vector<int> h(n);
+  for(int &i:h) cin >> i;
+  int num=0,ma=0;
+  for(int i=0; i<n; ++i){
+    if(h[i]>ma) ma=h[i],num=i+1;
+  }
+  cout << num << endl;
   return 0;
 }

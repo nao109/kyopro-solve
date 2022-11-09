35d11633ca976aadc24297b719bfd34adaad1d6e
nao109 <80878102+nao109@users.noreply.github.com>
2022-11-10 01:32:23 +0900

    resolve abc276-d

--- atcoder/abc/abc276-d.cpp
+++ atcoder/abc/abc276-d.cpp
@@ -4,14 +4,7 @@ using ll = long long;
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
-int main(){
-  vector<ll> num2, num3;
-  ll d2=1, d3=1;
-  for(int i=1; i<31; ++i){
-    num2.push_back(d2), num3.push_back(d3);
-    d2*=2, d3*=3;
-  }
-  
+int main(){  
   ll n;
   cin >> n;
   vector<ll> a(n);
@@ -20,18 +13,21 @@ int main(){
   for(int i=1; i<n; ++i) g=gcd(g, a[i]);
   for(ll &i:a) i/=g;
 
-  int ans=0,div=0;
+  int ans=0,cnt=0;
 
   for(ll &i:a){
-    int ma2=0, ma3=0;
-    for(int j=0; j<30; ++j){
-      if(i%num2[j]==0) ma2=j;
-      if(i%num3[j]==0) ma3=j;
+    while(i%2==0){
+      i/=2;
+      ans++;
     }
-    if(i/num3[ma3]/num2[ma2]==1) div++, ans+=ma2+ma3;
+    while(i%3==0){
+      i/=3;
+      ans++;
+    }
+    if(i==1) cnt++;
   }
   
-  if(div==n) cout << ans << endl;
-  else cout << -1 << endl;
+  if(cnt<n) ans=-1;
+  cout << ans << endl;
   return 0;
 }

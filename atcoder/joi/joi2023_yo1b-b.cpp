2713aba88cd7eb1081ef05ddac861b8c94cdfb17
nao109 <haruki.2004.109@gmail.com>
2022-11-17 14:54:00 +0900

    solve joi2023_yo1b-a,b,c,d

---
+++ atcoder/joi/joi2023_yo1b-b.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int x;
+  cin >> x;
+  cout << 24*x << endl;
+  return 0;
+}

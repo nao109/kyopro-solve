01caeb591e3c613ad6374b0f92b374dc76edb56f
nao109 <haruki.2004.109@gmail.com>
2022-11-17 14:53:43 +0900

    solve joi2023_yo1a-a,b,c,d during the contest

---
+++ atcoder/joi/joi2023_yo1a-b.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int n;
+  cin >> n;
+  if(n%10==n/10) cout << 1;
+  else cout << 0;
+  cout << endl;
+  return 0;
+}

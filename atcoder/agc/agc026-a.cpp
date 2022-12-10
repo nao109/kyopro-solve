feae98eea46fb7c862d589f5367974a931725d81
nao109 <haruki.2004.109@gmail.com>
2022-12-10 23:46:03 +0900

    solve agc026-a

--- atcoder/agc/agc026-a.cpp
+++ atcoder/agc/agc026-a.cpp
@@ -4,7 +4,6 @@ using ll = long long;
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
-const ll INF = 1000000000000000000;
 int main(){
   int n;
   cin >> n;
@@ -12,7 +11,7 @@ int main(){
   for(int &i:a) cin >> i;
 
   int ans=0;
-  for(int i=1; i<n-1; ++i){
+  for(int i=1; i<n; ++i){
     if(a[i-1]==a[i]) a[i]=10000,ans++;
   }
   cout << ans << endl;

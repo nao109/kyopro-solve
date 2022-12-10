ca801163a83b6db2ebda215bb7b8b12cdf78552d
nao109 <haruki.2004.109@gmail.com>
2022-12-10 23:40:02 +0900

    work on agc025-a

---
+++ atcoder/agc/agc025-a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int n;
+  cin >> n;
+  int mi=46;
+  for(int i=1; i<n/2; ++i){
+    int chk=0;
+    string a=to_string(i),b=to_string(n-i);
+    for(char &i:a) chk+=i-'0';
+    for(char &i:b) chk+=i-'0';
+    mi=min(chk,mi);
+  }
+  cout << mi << endl;
+  return 0;
+}

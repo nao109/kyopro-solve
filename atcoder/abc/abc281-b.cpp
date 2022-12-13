725e938bbb447c2eb15d51287e0a1e51d4e909cb
nao109 <haruki.2004.109@gmail.com>
2022-12-13 15:39:36 +0900

    resolve abc281-b

--- atcoder/abc/abc281-b.cpp
+++ atcoder/abc/abc281-b.cpp
@@ -7,22 +7,18 @@ using ll = long long;
 int main(){
   string s;
   cin >> s;
-  if(s.size()==8){
-    if('A'<=s[0] && s[0]<='Z' && 'A'<=s[7] && s[7]<='Z'){
-      for(int i=2; i<7; ++i){
-        if('1'<=s[1] && s[i]<='9'){
-          bool flag=true;
-          for(int i=2; i<7; ++i){
-            if(s[i]<'0' || '9'<s[i]) flag=false;
-          }
-          if(flag){
-            cout << "Yes\n";
-            return 0;
-          }
-        }
+  if(s.size()!=8) cout << "No\n";
+  else if(!('A'<=s[0] && s[0]<='Z')) cout << "No\n";
+  else if(!('A'<=s[7] && s[7]<='Z')) cout << "No\n";
+  else if(!('1'<=s[1] && s[1]<='9')) cout << "No\n";
+  else{
+    for(int i=2; i<7; ++i){
+      if(!('0'<=s[i] && s[i]<='9')){
+        cout << "No\n";
+        return 0;
       }
     }
+    cout << "Yes\n";
   }
-  cout << "No\n";
   return 0;
 }

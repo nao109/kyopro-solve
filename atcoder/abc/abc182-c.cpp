64d506176191bf2f35c902fa2bfe006752db8402
nao109 <80878102+nao109@users.noreply.github.com>
2022-12-15 23:09:42 +0900

    rework on abc182-c

--- atcoder/abc/abc182-c.cpp
+++ atcoder/abc/abc182-c.cpp
@@ -18,11 +18,11 @@ int main(){
   if(sum%3==0) ans=0;
   else if(sum%3==1){
     if(cnt[1]>0) ans=1,cnt[1]--;
-    else cnt[2]-=2;
+    else ans=2,cnt[1]-=2;
   }
   else{
-    if(cnt[2]>0) ans=1;
-    else cnt[1]-=2;
+    if(cnt[2]>0) ans=1,cnt[2]--;
+    else ans=2,cnt[1]-=2;
   }
 
   if(cnt[0]>0 || cnt[1]>0 || cnt[2]>0) cout << ans << endl;

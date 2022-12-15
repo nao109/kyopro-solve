f95fbcb3a5feb7677dbdb91a9d365e42ac939237
nao109 <80878102+nao109@users.noreply.github.com>
2022-12-15 23:09:02 +0900

    solve abc103-b,178-b,diverta2019-a

---
+++ atcoder/other/diverta2019-a.cpp
@@ -0,0 +1,12 @@
+#includebitsstdc++.h
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int n,k;
+  cin  n  k;
+  cout  n-k+1  endl;
+  return 0;
+}

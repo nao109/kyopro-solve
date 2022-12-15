25313a525eced6321d98be1dd69ee9135c9b72c2
nao109 <80878102+nao109@users.noreply.github.com>
2022-12-15 22:52:51 +0900

    solve arc131-a with editorial

---
+++ atcoder/arc/arc131-a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+#define all(a) a.begin(),a.end()
+int main(){
+  int a,b;
+  cin >> a >> b;
+  cout << a << 0 << b*5 << endl;
+  return 0;
+}

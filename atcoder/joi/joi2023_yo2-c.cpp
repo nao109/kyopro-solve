3fb272773c5f3b7cda0f16b4153f228070cf2391
nao109 <haruki.2004.109@gmail.com>
2022-12-16 13:35:04 +0900

    rework on joi2023_yo2-c 2

--- atcoder/joi/joi2023_yo2-c.cpp
+++ atcoder/joi/joi2023_yo2-c.cpp
@@ -5,19 +5,18 @@ using ll = long long;
 #define se second
 #define all(a) a.begin(),a.end()
 #define rep(i,n) for(int i=0; i<(n); ++i)
-const int INF = 1000000000;
 int main(){
   int h,w;
   cin >> h >> w;
+  vector<vector<int>> c(h,vector<int>(w));
+  rep(i,h)rep(j,w) cin >> c[i][j];
   
   if(h==1){
-    vector<int> c(w);
-    for(int i=0; i<w; ++i) cin >> c[i];
     vector<pair<int,int>> cr;
-    cr.emplace_back(c[0],1);
+    cr.emplace_back(c[0][0],1);
     for(int i=1; i<w; ++i){
-      if(c[i]==cr.back().fi) cr.back().se++;
-      else cr.emplace_back(c[i],1);
+      if(c[0][i]==cr.back().fi) cr.back().se++;
+      else cr.emplace_back(c[0][i],1);
     }
     int n=cr.size();
     int ans=cr[0].se;

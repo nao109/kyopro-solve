f7a73fdff60db913da1f37cae2afa3cc1d4b1fd6
nao109 <80878102+nao109@users.noreply.github.com>
2022-12-16 22:02:15 +0900

    rework on agc004-a

--- atcoder/agc/agc004-a.cpp
+++ atcoder/agc/agc004-a.cpp
@@ -10,6 +10,6 @@ int main(){
   cin >> a[0] >> a[1] >> a[2];
   sort(a,a+3);
   if(a[0]%2==0 || a[1]%2==0 || a[2]%2==0) cout << 0 << endl;
-  else cout << a[0]*a[1]*((a[2]+1)/2-a[2]/2) << endl;
+  else cout << a[0]*a[1] << endl;
   return 0;
 }

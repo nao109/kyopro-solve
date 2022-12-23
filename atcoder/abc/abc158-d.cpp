54ce1a83ca545c4c71625f59b6dbbf89c27f9778
nao109 <80878102+nao109@users.noreply.github.com>
2022-12-23 15:33:57 +0900

    rework on abc158-d

--- atcoder/abc/abc158-d.cpp
+++ atcoder/abc/abc158-d.cpp
@@ -8,20 +8,28 @@ int main(){
   string s;
   int q;
   cin >> s >> q;
-  bool swap=false;
-  for(int i=0; i<q; i++){
-    int k;
-    cin >> k;
-    if(k==1) swap=!swap;
+  
+  bool flag=false;
+  for(int i=0; i<q; ++i){
+    int x;
+    cin >> x;
+    if(x==1) flag=!flag;
     else{
       int f;
       char c;
       cin >> f >> c;
-      if((!swap && f==1) || (swap && f==2)) s=c+s;
-      else s=s+c;
+      if(flag){
+        if(f==1) s=c+s;
+        else s=s+c;
+      }
+      else{
+        if(f==1) s=s+c;
+        else s=c+s;
+      }
     }
   }
-  if(swap) reverse(all(s));
+  
+  if(!flag) reverse(all(s));
   cout << s << endl;
   return 0;
 }

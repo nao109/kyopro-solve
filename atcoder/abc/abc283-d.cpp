07aee92c2adc3e101eee8a595b4026f00b1cbe02
nao109 <80878102+nao109@users.noreply.github.com>
2022-12-26 10:57:55 +0900

    upsolve abc283-d with editorial

--- atcoder/abc/abc283-d.cpp
+++ atcoder/abc/abc283-d.cpp
@@ -9,6 +9,7 @@ int main(){
   cin >> s;
   int n=s.size();
   vector<set<char>> box(1);
+  vector<bool> used(128);
   int c=0;
   for(int i=0; i<n; ++i){
     if(s[i]=='('){
@@ -16,15 +17,19 @@ int main(){
       c++;
     }
     else if(s[i]==')'){
+      for(char x:box[c]) used[x]=false;
       box[c].clear();
       c--;
     }
     else{
-      if(box[c].count(s[i])){
+      if(used[s[i]]){
         cout << "No\n";
         return 0;
       }
-      else box[c].insert(s[i]);
+      else{
+        box[c].insert(s[i]);
+        used[s[i]]=true;
+      }
     }
   }
   cout << "Yes\n";

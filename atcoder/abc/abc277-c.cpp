284b9ad4ad9d71727033f5f05e4a9754cc956a90
nao109 <80878102+nao109@users.noreply.github.com>
2023-01-04 23:29:26 +0900

    upsolve abc277-c with editorial

--- atcoder/abc/abc277-c.cpp
+++ atcoder/abc/abc277-c.cpp
@@ -5,59 +5,33 @@ using ll = long long;
 #define se second
 #define all(a) a.begin(),a.end()
 
-vector<int> cc;
-void dfs(vector<vector<int>> &g,int v,int id){
-  cc[v]=id;
-  for(auto e:g[v]){
-    if(cc[e]==-1){
-      dfs(g,e,id);
-    }
+vector<bool> seen;
+void dfs(map<int,vector<int>> &g,int v){
+  seen[v]=true;
+  for(int next_v:g[v]){
+    if(!seen[next_v]) dfs(g,next_v);
   }
 }
 
-int main() {
+int main(){
   int n;
   cin >> n;
-  set<int> fl;
-  vector<pair<int,int>> r;
+  map<int,vector<int>> g;
   for(int i=0; i<n; ++i){
     int a,b;
     cin >> a >> b;
-    --a,--b;
-    fl.insert(a),fl.insert(b);
-    r.emplace_back(a,b);
-    r.emplace_back(b,a);
+    a--,b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
   }
 
-  if(!fl.count(0)){
-    cout << 1 << endl;
-    return 0;
+  seen.assign(1000000000,false);
+  dfs(g,0);
+  
+  int ans=0;
+  for(int i=0; i<1000000000; ++i){
+    if(seen[i]) ans=i+1;
   }
-
-  int s=fl.size();
-  int cnt=0;
-  map<int,int> num,rev;
-  for(int i:fl) num[i]=cnt,rev[cnt]=i,++i;
-  
-  vector<vector<int>> g(s);
-  for(auto i:num) g[r[i.se].fi].push_back(r[i.se].se);
-  
-  cc.assign(s,-1);
-  int id=0;
-  for(int i=0; i<n; ++i){
-    if(cc[i]==-1){
-      dfs(g,i,id);
-      id++;
-    }
-  }
-  
-  int ma=0;
-  for(auto i:rev){
-    if(cc[0]==cc[i.fi]){
-      ma=i.se;
-    }
-  }
-  
-  cout << ma+1 << endl;
+  cout << ans << endl;
   return 0;
 }

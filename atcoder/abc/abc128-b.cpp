5741ccb1157839c68adcb85027bad8351ac2ac8a
nao109 <haruki.2004.109@gmail.com>
2023-01-12 16:02:19 +0900

    resolve abc128-b

--- atcoder/abc/abc128-b.cpp
+++ atcoder/abc/abc128-b.cpp
@@ -1,21 +1,21 @@
-#include<bits/stdc++.h>
+	#include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); ++i)
 #define fi first
 #define se second
-#define pb push_back
 #define all(a) a.begin(),a.end()
-using P = pair<string,int>;
 int main(){
-  cout << fixed << setprecision(15);
-  int n; cin >> n;
-  vector<pair<P,int>> a(n);
-  rep(i,n){
-    cin >> a[i].fi.fi >> a[i].fi.se;
-    a[i].fi.se*=-1; a[i].se=i+1;
+  int n;
+  cin >> n;
+  vector<pair<pair<string,int>,int>> spi(n);
+  for(int i=0; i<n; ++i){
+    cin >> spi[i].fi.fi >> spi[i].fi.se;
+    spi[i].fi.se*=-1;
+    spi[i].se=i+1;
   }
-  sort(all(a));
-  rep(i,n) cout << a[i].se << endl;
+  sort(all(spi));
+  for(int i=0; i<n; ++i){
+    cout << spi[i].se << endl;
+  }
   return 0;
 }

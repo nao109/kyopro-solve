6cdf3cdf2d8604a97a2ee29895ce75d7f72c8670
nao109 <80878102+nao109@users.noreply.github.com>
2023-01-21 01:17:52 +0900

    solve abc221-c with editorial

--- atcoder/abc/abc221-c.cpp
+++ atcoder/abc/abc221-c.cpp
@@ -7,17 +7,21 @@ using ll = long long;
 int main(){
     string s;
     cin >> s;
+    sort(all(s));
     int n = s.size();
 
     int ma = 0;
     
-    for(int i = 1; i < n; ++i){
-        if(s[i]!='0'){
-            int a = stoi(s.substr(0, i));
-            int b = stoi(s.substr(i));
-            ma = max(a * b, ma);
+    do{
+        for(int i = 1; i < n; ++i){
+            if(s[i]!='0'){
+                int a = stoi(s.substr(0, i));
+                int b = stoi(s.substr(i));
+                ma = max(a * b, ma);
+            }
         }
-    }
+    }while(next_permutation(all(s)));
+    
 
     cout << ma << endl;
     return 0;

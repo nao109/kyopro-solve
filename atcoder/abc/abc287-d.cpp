043579b1d523251d6c5a1a015af73978f6370cbb
nao109 <80878102+nao109@users.noreply.github.com>
2023-01-29 00:25:28 +0900

    work on abc287-d

--- atcoder/abc/abc287-d.cpp
+++ atcoder/abc/abc287-d.cpp
@@ -6,21 +6,19 @@ using ll = long long;
 int main(){
     string s, t;
     cin >> s >> t;
+    int ss = s.size();
     int ts = t.size();
 
-    for(int i = 0; i <= ts; ++i){
-        bool flag = true;
-        string ss = s.substr(0, i) + s.substr(i + 1, ts - i);
-        if(ss[0] != '?' && t[0] != '?' && ss[0] != t[0]){
-            cout << "No\n";
-            continue;
-        }
-        for(int j = 1; j < ts; ++j){
-            if(ss[j] == '?' || t[j] == '?') continue;
-            if(ss[j] != t[j]) flag = false;
-        }
+    int cnt = 0;
+    for(int i = 0; i < ts; ++i){
+        if(s[ss - ts + i] == '?' || t[i] == '?' || s[ss - ts + i] == t[i]) cnt++;
+    }
+    cout << (cnt == ts ? "Yes\n" : "No\n");
 
-        cout << (flag ? "Yes\n" : "No\n");
+    for(int i = 0; i < ts; ++i){
+        cnt--;
+        if(s[i] == '?' || t[i] == '?' || s[i] == t[i]) cnt++;
+        cout << (cnt == ts ? "Yes\n" : "No\n");
     }
     return 0;
 }

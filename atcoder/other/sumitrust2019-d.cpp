2ca694bc30cc4b8f3102388003c0ceb711d9e67c
nao109 <haruki.2004.109@gmail.com>
2023-03-03 12:22:45 +0900

    solve sumitrust2019-d

--- atcoder/other/sumitrust2019-d.cpp
+++ atcoder/other/sumitrust2019-d.cpp
@@ -1,26 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
 int main(){
-  int n;
-  string s;
-  cin >> n >> s;
-  set<string> cnt;
-  for(int i=0; i<n; ++i){
-    for(int j=i+1; j<n; ++j){
-      for(int k=j+1; k<n; ++k){
-        string str;
-        str=s[i];
-        str+=s[j];
-        str+=s[k];
-        if(!cnt.count(str)) cnt.insert(str);
-        else continue;
-      }
+    int n;
+    string s;
+    cin >> n >> s;
+    int ans = 0;
+    for(int i = 0; i < 1000; ++i){
+        string t;
+        if(i < 10) t = "00" + to_string(i);
+        else if(i < 100) t = "0" + to_string(i);
+        else t = to_string(i);
+        int chk = 0;
+        for(int i = 0; i < n; ++i){
+            if(chk < 3 && t[chk] == s[i]) chk++;
+        }
+        if(chk == 3) ans++;
     }
-  }
-  cout << cnt.size() << endl;
-  return 0;
+    cout << ans << endl;
+    return 0;
 }

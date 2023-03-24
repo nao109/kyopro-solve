66c49550f2ab1cb71f848c386f0812f3e14821e5
nao109 <haruki.2004.109@gmail.com>
2023-03-24 23:47:05 +0900

    solve abc177-c

--- atcoder/abc/abc177-c.cpp
+++ atcoder/abc/abc177-c.cpp
@@ -7,19 +7,15 @@ const ll mod = 1000000007;
 int main(){
     ll n;
     cin >> n;
-    vector<ll> a(n + 1);
-    for(int i = 1; i <= n; ++i){
+    vector<ll> a(n), s(n + 1);
+    for(int i = 0; i < n; ++i){
         cin >> a[i];
-    }
-
-    vector<ll> s(n + 1);
-    for(int i = 1; i <= n; ++i){
-        s[i] = s[i - 1] + a[i];
+        s[i + 1] = s[i] + a[i];
     }
 
     ll ans = 0;
-    for(int i = 1; i <= n; ++i){
-        ans = (ans + a[i] * (s[n] - s[i])) % mod;
+    for(int i = 0; i < n; ++i){
+        ans = (ans + a[i] * ((s[n] - s[i + 1]) % mod)) % mod;
     }
     cout << ans << endl;
     return 0;

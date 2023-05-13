18995b78506dd2f820ddecf3113ad16155cfd0a1
nao109 <80878102+nao109@users.noreply.github.com>
2023-05-14 01:26:55 +0900

    upsolve abc301-c

--- atcoder/abc/abc301-c.cpp
+++ atcoder/abc/abc301-c.cpp
@@ -4,80 +4,35 @@ using ll = long long;
 #define fi first
 #define se second
 int main(){
-    set<char> atcoder;
-    atcoder.insert('a');
-    atcoder.insert('t');
-    atcoder.insert('c');
-    atcoder.insert('o');
-    atcoder.insert('d');
-    atcoder.insert('e');
-    atcoder.insert('r');
-
     string s, t;
     cin >> s >> t;
 
-    map<char, int> ss, ts;
+    map<char, int> scnt, tcnt;
     for(char i : s){
-        if(!ts.count(i)) ts[i] = 0;
-
-        if(ss.count(i)) ss[i]++;
-        else ss[i] = 1;
+        if(scnt.count(i)) scnt[i]++;
+        else scnt[i] = 1;
     }
     for(char i : t){
-        if(!ss.count(i)) ss[i] = 0;
-
-        if(ts.count(i)) ts[i]++;
-        else ts[i] = 1;
+        if(tcnt.count(i)) tcnt[i]++;
+        else tcnt[i] = 1;
     }
 
-    for(auto [i, num] : ss){
-        if(i == '@') continue;
+    for(char i : "atcoder"){
+        int m = max(scnt[i], tcnt[i]);
 
-        int pre = min(ss[i], ts[i]);
-        ss[i] -= pre, ts[i] -= pre;
-    }
-    for(auto [i, num] : ts){
-        if(i == '@') continue;
-
-        int pre = min(ss[i], ts[i]);
-        ss[i] -= pre, ts[i] -= pre;
-    }
-
-    // for(auto i : ss) cout << i.fi << " " << i.se << endl;
-    // for(auto i : ts) cout << i.fi << " " << i.se << endl;
-
-    for(auto [i, num] : ss){
-        if(i == '@' || !atcoder.count(i)) continue;
-        
-        if(ss[i] < ts[i]){
-            ss['@'] -= ts[i] - ss[i];
-            ss[i] = ts[i];
+        if(scnt['@'] < m - scnt[i] || tcnt['@'] < m - tcnt[i]){
+            cout << "No\n";
+            return 0;
         }
-        else if(ss[i] > ts[i]){
-            ts['@'] -= ss[i] - ts[i];
-            ts[i] = ss[i];
-        }
+        scnt['@'] -= m - scnt[i];
+        scnt[i] = m;
+        tcnt['@'] -= m - tcnt[i];
+        tcnt[i] = m;
     }
-
-    for(auto [i, num] : ts){
-        if(i == '@' || !atcoder.count(i)) continue;
-        
-        if(ss[i] < ts[i]){
-            ss['@'] -= ts[i] - ss[i];
-            ss[i] = ts[i];
-        }
-        else if(ss[i] > ts[i]){
-            ts['@'] -= ss[i] - ts[i];
-            ts[i] = ss[i];
-        }
-    }
-
-    // for(auto i : ss) cout << i.fi << " " << i.se << endl;
-    // for(auto i : ts) cout << i.fi << " " << i.se << endl;
 
     bool flag = true;
-    for(auto [i, num] : ss){
-        if(ss[i] != ts[i]) flag = false;
+    for(char i : s + t){
+        if(scnt[i] != tcnt[i]) flag = false;
     }
     cout << (flag ? "Yes\n" : "No\n");
     return 0;

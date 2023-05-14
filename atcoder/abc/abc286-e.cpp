0c503cc1572e1b45e2b7cf8fb815cb59efc20034
nao109 <80878102+nao109@users.noreply.github.com>
2023-05-14 18:32:52 +0900

    solve abc286-e

--- atcoder/abc/abc286-e.cpp
+++ atcoder/abc/abc286-e.cpp
@@ -6,7 +6,7 @@ using ll = long long;
 int main(){
     int n;
     cin >> n;
-    vector<int> a(n);
+    vector<ll> a(n);
     vector<string> s(n);
     for(int i = 0; i < n; i++) cin >> a[i];
     for(int i = 0; i < n; i++) cin >> s[i];
@@ -18,36 +18,37 @@ int main(){
         }
     }
 
-    int q;
-    cin >> q;
-    for(int i = 0; i < q; i++){
-        int j, k;
-        cin >> j >> k;
-        j--, k--;
-        
-        vector<int> dist(n, -1), value(n, -1);
+    vector<vector<int>> dist(n, vector<int>(n, -1));
+    vector<vector<ll>> value(n, vector<ll>(n, -1));
+    for(int i = 0; i < n; i++){
         queue<int> que;
-        dist[j] = 0;
-        value[j] = a[j];
-        que.push(j);
+        dist[i][i] = 0;
+        value[i][i] = a[i];
+        que.push(i);
 
         while(!que.empty()){
             int v = que.front();
             que.pop();
             for(int nv : g[v]){
-                if(dist[nv] == -1){
-                    dist[nv] = dist[v] + 1;
-                    value[nv] = value[v] + a[nv];
+                if(dist[i][nv] == -1){
+                    dist[i][nv] = dist[i][v] + 1;
+                    value[i][nv] = value[i][v] + a[nv];
                     que.push(nv);
                 }
-                else if(dist[nv] == dist[v] + 1){
-                    value[nv] = max(value[nv], value[v] + a[nv]);
+                else if(dist[i][nv] == dist[i][v] + 1){
+                    value[i][nv] = max(value[i][nv], value[i][v] + a[nv]);
                 }
             }
         }
-
-        if(dist[k] == -1) cout << "Impossible\n";
-        else cout << dist[k] << " " << value[k] << endl;
+    }
+    
+    int q;
+    cin >> q;
+    for(int i = 0; i < q; i++){
+        int u, v;
+        cin >> u >> v;
+        if(dist[u - 1][v - 1] == -1) cout << "Impossible\n";
+        else cout << dist[u - 1][v - 1] << " " << value[u - 1][v - 1] << endl;
     }
     return 0;
 }

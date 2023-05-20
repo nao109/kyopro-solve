7db446909a2a46d225bc656425c002b31f9ef39c
nao109 <80878102+nao109@users.noreply.github.com>
2023-05-20 23:17:23 +0900

    solve abc302-a,b,c during the contest

---
+++ atcoder/abc/abc302-a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+int main(){
+    ll a, b;
+    cin >> a >> b;
+    cout << (a + b - 1) / b << endl;
+    return 0;
+}

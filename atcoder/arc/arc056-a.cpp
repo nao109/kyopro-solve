1c496df0e665c40883b5d121e553c7b2019ec3ab
nao109 <haruki.2004.109@gmail.com>
2023-06-07 00:19:41 +0900

    solve arc056-a and replace arc011-a,022-a

--- atcoder/arc/arc056-a.cpp
+++ atcoder/arc/arc056-a.cpp
@@ -1,14 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
-#define pi acos(-1)
 int main(){
-  int a,b,k,l;
-  cin >> a >> b >> k >> l;
-  int sum=min(((k+l-1)/l)*b,(k/l)*b+(k%l)*a);
-  cout << sum << endl;
-  return 0;
+    ll a, b, k, l;
+    cin >> a >> b >> k >> l;
+    ll ans = 0;
+    ans = (k / l) * b + min(b, (k % l) * a);
+    cout << ans << endl;
+    return 0;
 }

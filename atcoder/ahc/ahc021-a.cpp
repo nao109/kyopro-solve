7842198171953fe8b51085c3251f3bc4b8944a22
nao109 <80878102+nao109@users.noreply.github.com>
2023-06-26 11:07:32 +0900

    work on ahc021-a

---
+++ atcoder/ahc/ahc021-a.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+
+using pii = pair<int, int>;
+
+vector<pair<pii, pii>> solve(vector<vector<int>> &b){
+    vector<pair<pii, pii>> res;
+    for(int i = 0; i < 29; i++){
+        for(int j = 0; j < i; j++){
+            if(b[i][j] > b[i + 1][j]){
+                swap(b[i][j], b[i + 1][j]);
+                res.push_back({{i, j}, {i + 1, j}});
+            }
+        }
+    }
+    return res;
+}
+
+int main(){
+    vector<vector<int>> b(30);
+    for(int i = 0; i < 30; i++){
+        for(int j = 0; j < i; j++){
+            int x;
+            cin >> x;
+            b[i].push_back(x);
+        }
+    }
+    
+    vector<pair<pii, pii>> ans = solve(b);
+    int k = ans.size();
+    cout << k << endl;
+    for(int i = 0; i < k; i++) cout << ans[i].fi.fi << " " << ans[i].fi.se << " " << ans[i].se.fi << " " << ans[i].se.se << endl;
+    return 0;
+}

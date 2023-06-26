308e9b275236060270b2530cd9d654092223ff3d
nao109 <haruki.2004.109@gmail.com>
2023-06-26 11:15:25 +0900

    rework on ahc021-a

--- atcoder/ahc/ahc021-a.cpp
+++ atcoder/ahc/ahc021-a.cpp
@@ -8,13 +8,27 @@ using pii = pair<int, int>;
 
 vector<pair<pii, pii>> solve(vector<vector<int>> &b){
     vector<pair<pii, pii>> res;
-    for(int i = 0; i < 29; i++){
-        for(int j = 0; j <= i; j++){
-            if(b[i][j] > b[i + 1][j]){
-                swap(b[i][j], b[i + 1][j]);
-                res.push_back({{i, j}, {i + 1, j}});
+    while(1){
+        int cnt = 0;
+        for(int i = 0; i < 29; i++){
+            for(int j = 0; j <= i; j++){
+                if(b[i][j] > b[i + 1][j]){
+                    swap(b[i][j], b[i + 1][j]);
+                    res.push_back({{i, j}, {i + 1, j}});
+                    cnt++;
+                }
             }
         }
+        for(int i = 0; i < 29; i++){
+            for(int j = 0; j <= i; j++){
+                if(b[i][j] > b[i + 1][j + 1]){
+                    swap(b[i][j], b[i + 1][j + 1]);
+                    res.push_back({{i, j}, {i + 1, j + 1}});
+                    cnt++;
+                }
+            }
+        }
+        if(cnt == 0) break;
     }
     return res;
 }

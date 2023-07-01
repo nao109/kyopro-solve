c482d010938d16e4191a0df118c35113ea46cdbe
nao109 <haruki.2004.109@gmail.com>
2023-07-01 17:30:11 +0900

    work on abc035-b

--- atcoder/abc/abc035-b.cpp
+++ atcoder/abc/abc035-b.cpp
@@ -1,38 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); ++i)
 #define fi first
 #define se second
-#define pb push_back
-#define all(a) a.begin(),a.end()
-#define pi acos(-1)
 int main(){
-  cout << fixed << setprecision(15);
-  string s; int t; cin >> s >> t;
-  int n=s.size();
-  int x=0,y=0,cnt=0;
-  rep(i,n){
-    if(s[i]=='L') --x;
-    else if(s[i]=='R') ++x;
-    else if(s[i]=='U') ++y;
-    else if(s[i]=='D') --y;
-    else ++cnt;
-  }
-  if(t==1){
-    if(x>=0) x+=cnt;
-    else x-=cnt;
-  }
-  else{
-    if(x>=cnt){x-=cnt; cnt=0;}
-    else if(0<=x&&x<cnt){cnt-=x; x=0;}
-    else if(-cnt<x&&x<0){cnt-=x; x=0;}
-    else if(x<=-cnt){x+=cnt; cnt=0;}
-    if(cnt>0){
-      if(0<=y) y-=cnt;
-      else y+=cnt;
+    string s;
+    int t;
+    cin >> s >> t;
+
+    pair<int, int> now = {0, 0};
+    int cnt = 0;
+
+    for(char i : s){
+        if(i == 'L') now.fi--;
+        if(i == 'R') now.fi++;
+        if(i == 'U') now.se++;
+        if(i == 'D') now.se--;
+        if(i == '?') cnt++;
     }
-  }
-  cout << abs(x)+abs(y) << endl;
-  return 0;
+
+    if(t == 1) now.fi += (now.fi >= 0 ? cnt : -cnt);
+    else{
+        if(now.fi != 0){
+            if(cnt < abs(now.fi)){
+                now.fi += (now.fi > 0 ? -cnt : cnt);
+                cnt = 0;
+            }
+            else{
+                cnt -= abs(now.fi);
+                now.fi = 0;
+            }
+        }
+
+        if(now.se == 0) now.se = cnt % 2;
+        else if(now.se > 0) now.se += (now.se > 0 ? -cnt : cnt);
+        cnt = 0;
+    }
+
+    cout << abs(now.fi) + abs(now.se) << endl;
+    return 0;
 }

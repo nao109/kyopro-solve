58559674cefe3a6dd1c3c21eb31c323b21dc4de8
nao109 <haruki.2004.109@gmail.com>
2023-07-23 15:34:22 +0900

    upsolve abc258-c

--- atcoder/abc/abc258-c.cpp
+++ atcoder/abc/abc258-c.cpp
@@ -1,19 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
 int main(){
-  int n,q;
-  string s;
-  cin >> n >> q >> s;
-  int start=0;
-  for(int i=0; i<q; ++i){
-    int k,x;
-    cin >> k >> x;
-    if(k==1) start=(start-x+n)%n;
-    else cout << s[(start+x)%n-1] << endl;
-  }
-  return 0;
+    int n, q;
+    string s;
+    cin >> n >> q >> s;
+    int first = 0;
+    while(q--){
+        int t, x;
+        cin >> t >> x;
+        if(t == 1) first = (first + n - x) % n;
+        else cout << s[(first + x - 1 + n) % n] << endl;
+    }
+    return 0;
 }

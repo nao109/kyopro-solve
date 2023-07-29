a9f4ea26a03d1fbd2d32e76c05ae301c4a0f2514
nao109 <haruki.2004.109@gmail.com>
2023-07-29 14:07:14 +0900

    solve keyence2019-b

--- atcoder/other/keyence2019-b.cpp
+++ atcoder/other/keyence2019-b.cpp
@@ -1,20 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); ++i)
 #define fi first
 #define se second
-#define pb push_back
 int main(){
-  cout << fixed << setprecision(7);
-  string s,ch="keyence"; cin >> s;
-  int ss=s.size(),cnt=0;
-  vector<string> rid(1000);
-  rep(i,ss){
-    if(s[i]==ch[cnt]&&cnt<7) ++cnt;
-    else rid[cnt].pb(s[i]);
-  }
-  if(cnt==7&&rid.size()==1) cout << "YES\n";
-  else cout << "NO\n";
-  return 0;
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    string keyence = "keyence";
+    int cnt = 7;
+    for(int i = 1; i <= 7; i++){
+        if(s.substr(0, i) == keyence.substr(0, i)) cnt--;
+    }
+    cout << ((s.substr(n - cnt) == keyence.substr(7 - cnt)) ? "YES\n" : "NO\n");
+    return 0;
 }

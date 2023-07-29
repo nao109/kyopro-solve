2542e8294082c0941548edfe3b3bb2f9ac9af014
nao109 <80878102+nao109@users.noreply.github.com>
2023-07-30 00:21:59 +0900

    solve abc312-a,b,c during the contest

---
+++ atcoder/abc/abc312-a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+int main(){
+    string s;
+    cin >> s;
+    if(s == "ACE" || s == "BDF" || s == "CEG" || s == "DFA" || s == "EGB" || s == "FAC" || s == "GBD") cout << "Yes\n";
+    else cout << "No\n";
+    return 0;
+}

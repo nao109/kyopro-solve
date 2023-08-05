7e115582502d73449c4a5700b93f16e60bc1f1b3
nao109 <80878102+nao109@users.noreply.github.com>
2023-08-05 16:58:29 +0900

    solve abc243-d

--- atcoder/abc/abc243-d.cpp
+++ atcoder/abc/abc243-d.cpp
@@ -1,19 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
 int main(){
-  int n;
-  ll x;
-  string s;
-  cin >> n >> x >> s;
-  for(char i:s){
-    if(i=='U') x/=2;
-    else if(i=='L') x*=2;
-    else x=x*2+1;
-  }
-  cout << x << endl;
-  return 0;
+    ll n, x;
+    string s;
+    cin >> n >> x >> s;
+
+    stack<char> st;
+    for(int i = 0; i < n; i++){
+        if(st.empty()) st.push(s[i]);
+        else if((st.top() == 'L' || st.top() == 'R') && s[i] == 'U') st.pop();
+        else st.push(s[i]);
+    }
+
+    string t = "";
+    while(!st.empty()){
+        t.push_back(st.top());
+        st.pop();
+    }
+    reverse(t.begin(), t.end());
+
+    for(int i = 0; i < (int)t.size(); i++){
+        if(t[i] == 'U') x /= 2;
+        else if(t[i] == 'L') x *= 2;
+        else x = x * 2 + 1;
+    }
+
+    cout << x << endl;
+    return 0;
 }

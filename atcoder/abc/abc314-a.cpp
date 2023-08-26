0ff15db23e1c5823d9ee9f5d995b4d5903fc6230
nao109 <80878102+nao109@users.noreply.github.com>
2023-08-26 23:02:53 +0900

    solve abc314-a,b,c

---
+++ atcoder/abc/abc314-a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+int main(){
+    string pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    int n;
+    cin >> n;
+    cout << pi.substr(0, n + 2) << endl;
+    return 0;
+}

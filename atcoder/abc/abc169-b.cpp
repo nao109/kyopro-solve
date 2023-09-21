ca5474d820ced12125caefc8211b252aad2519b9
nao109 <80878102+nao109@users.noreply.github.com>
2023-09-21 13:03:10 +0900

    resolve abc169-b

--- atcoder/abc/abc169-b.cpp
+++ atcoder/abc/abc169-b.cpp
@@ -1,26 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
 int main(){
-  int n;
-  cin >> n;
-  vector<ll> a(n);
-  for(ll &i:a) cin >> i;
-  sort(all(a));
-  if(a[0]==0){
-    cout << "0\n";
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    bool in0 = false;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] == 0) in0 = true;
+    }
+
+    if(in0){
+        cout << 0 << endl;
+        return 0;
+    }
+
+    ll a_max = 1e18;
+    for(int i = 0; i < n; i++) a_max /= a[i];
+    if(a_max < 1){
+        cout << -1 << endl;
+        return 0;
+    }
+
+    ll ans = 1;
+    for(int i = 0; i < n; i++) ans *= a[i];
+    cout << ans << endl;
     return 0;
-  }
-  ll ch=1000000000000000000;
-  for(ll i:a) ch/=i;
-  if(ch==0) cout << "-1\n";
-  else{
-    ll ans=1;
-    for(ll &i:a) ans*=i;
-    cout << ans << "\n";
-  }
-  return 0;
 }

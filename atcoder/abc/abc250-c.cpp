22fd1d7029f33b8a112cdcd188eb60e1c9a84431
nao109 <80878102+nao109@users.noreply.github.com>
2023-11-18 22:58:56 +0900

    solve abc250-c

--- atcoder/abc/abc250-c.cpp
+++ atcoder/abc/abc250-c.cpp
@@ -1,33 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int i=0; i<(n); ++i)
 #define fi first
 #define se second
-#define pb push_back
-#define all(a) a.begin(),a.end()
-#define pi acos(-1)
 int main(){
-  cout << fixed << setprecision(15);
-  int n,q; cin >> n >> q;
-  vector<pair<int,int>> a(n);
-  rep(i,n){a[i+1].fi=i+1; a[i+1].se=i+1;}
-  rep(i,q){
-    int x; cin >> x;
-    sort(all(a));
-    auto itr=lower_bound(all(a),pair<int,int>{x,-1});
-    int d=distance(a.begin(), itr);
-    if(d<n){
-      swap(a[d].fi,a[d+1].fi);
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    vector<int> pos(n + 1);
+    for(int i = 0; i < n; i++){
+        a[i] = i + 1;
+        pos[i + 1] = i;
     }
-    else{
-      swap(a[d].fi,a[d-1].fi);
+
+    while(q--){
+        int x;
+        cin >> x;
+        if(pos[x] < n - 1){
+            swap(a[pos[x]], a[(pos[x] + 1) % n]);
+            pos[a[pos[x]]]--;
+            pos[x]++;
+        }
+        else{
+            swap(a[pos[x]], a[pos[x] - 1]);
+            pos[a[pos[x]]]++;
+            pos[x]--;
+        }
     }
-  }
-  rep(i,n){
-    cout << a[i].se;
-    if(i<n) cout << " ";
-    else cout << endl;
-  }
-  return 0;
+
+    
+    for(int i = 0; i < n; i++){
+        cout << a[i] << " \n"[i == n - 1];
+    }
+    return 0;
 }

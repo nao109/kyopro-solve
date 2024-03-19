3338464d36143442a9ae16550717567221113ff4
nao109 <80878102+nao109@users.noreply.github.com>
2024-03-19 22:06:55 +0900

    solve abc267-c with editorial

--- atcoder/abc/abc267-c.cpp
+++ atcoder/abc/abc267-c.cpp
@@ -1,23 +1,33 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
+
+template<typename T> void chmin(T& a, const T& b){ if(a > b) a = b; }
+template<typename T> void chmax(T& a, const T& b){ if(a < b) a = b; }
+
+const ll INF = 1e18;
+
 int main(){
-  ll n,m;
-  cin >> n >> m;
-  vector<ll> a(n);
-  for(ll &i:a) cin >> i;
-  int ma=LLONG_MIN;
-  for(int i=0; i<n-m+1; ++i){
-    int sum=0;
-    for(int j=0; j<m; ++j){
-      sum+=j*s[i+j];
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(n + 1);
+    for(ll i = 1; i <= n; i++) cin >> a[i];
+
+    vector<ll> sum(n + 1);
+    for(ll i = 1; i <= n; i++) sum[i] = sum[i - 1] + a[i];
+
+    ll ans = 0, max_sum = 0;
+    for(ll i = 1; i <= m; i++) max_sum += i * a[n - m + i];
+    ans = max_sum;
+
+    for(ll i = n; i >= m + 1; i--){
+        max_sum -= m * a[i];
+        max_sum += sum[i - 1] - sum[i - 1 - m];
+        chmax(ans, max_sum);
     }
-    cerr << sum << endl;
-    ma=max(sum,ma);
-  }
-  cout << ma << endl;
-  return 0;
+
+    cout << ans << "\n";
+    return 0;
 }

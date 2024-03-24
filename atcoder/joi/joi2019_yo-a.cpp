6a721e7d11174a5c64449d8094fefa04513ddcde
nao109 <80878102+nao109@users.noreply.github.com>
2024-03-25 00:02:58 +0900

    solve joi2019_yo-a

--- atcoder/joi/joi2019_yo-a.cpp
+++ atcoder/joi/joi2019_yo-a.cpp
@@ -1,18 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
+
 int main(){
-  int a,b,c;
-  cin >> a >> b >> c;
-  int d=c/(a*7+b)*7;
-  c%=a*7+b;
-  while(c>0){
-    c-=a;
-    ++d;
-  }
-  cout << d << endl;
-  return 0;
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int ans = 0;
+
+    while(c > 0){
+        c -= a;
+        if(ans % 7 == 6) c -= b;
+        ans++;
+    }
+
+    cout << ans << "\n";
+    return 0;
 }

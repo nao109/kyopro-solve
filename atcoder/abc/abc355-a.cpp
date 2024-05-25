cd4f2315bec058d80f11be254ec9f09be493d207
nao109 <haruki.2004.109@gmail.com>
2024-05-25 22:48:25 +0900

    solve abc355-a,b,c during the contest

---
+++ atcoder/abc/abc355-a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a == b) cout << "-1\n";
+    else cout << 6 - a - b << "\n";
+    return 0;
+}

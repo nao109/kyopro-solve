5eacec2d18e9d6f6410d6bdfa94b29338dbcb8be
nao109 <80878102+nao109@users.noreply.github.com>
2024-05-30 22:09:13 +0900

    solve math_and_algorithm-ai

--- atcoder/other/math_and_algorithm/math_and_algorithm-ai.cpp
+++ atcoder/other/math_and_algorithm/math_and_algorithm-ai.cpp
@@ -1,24 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-#define all(a) a.begin(),a.end()
+
 int main(){
-  int n,q;
-  cin >> n >> q;
-  vector<int> a(n),sum(n);
-  for(int i=0; i<n; ++i){
-    cin >> a[i];
-    if(i==0) sum[i]=a[i];
-    else sum[i]=sum[i-1]+a[i];
-  }
-  
-  for(int i=0; i<q; ++i){
-    int l,r;
-    cin >> l >> r;
-    --l,--r;
-    cout << sum[r]-sum[l-1] << endl;
-  }
-  return 0;
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    vector<int> s(n + 1);
+    for(int i = 0; i < n; i++) s[i + 1] = s[i] + a[i];
+
+    while(q--){
+        int l, r;
+        cin >> l >> r;
+        cout << s[r] - s[l - 1] << "\n";
+    }
+    return 0;
 }
